e46866791b469dd11342bec204a7b4f25a5c2abe
Adarsh Anand <adarshanand1436@gmail.com>
2026-09-05 21:38:04 +0530

    Delete sample_codebase directory

--- sample_codebase/secure_module.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-
-def encrypt_aes_gcm(data, key):
-    # AES-256-GCM is currently considered quantum resistant
-    # Migration to ML-KEM recommended for asymmetric parts
-    cipher = AES.new(key, AES.MODE_GCM)
-    ciphertext, tag = cipher.encrypt_and_digest(data)
-    return ciphertext
